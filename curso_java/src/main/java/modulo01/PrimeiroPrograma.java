2ea4b056291ca9a66acab498344f5fac0120ef31
Alex Jr <alexjunior2018@gmail.com>
2025-06-07 23:19:45 -0300

    initial commit

---
+++ curso_java/src/main/java/modulo01/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package modulo01;
+
+import javax.swing.*;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame();
+        janela.setTitle("Hello World!");
+        janela.setSize(500,500);
+        janela.setVisible(true);
+    }
+}
