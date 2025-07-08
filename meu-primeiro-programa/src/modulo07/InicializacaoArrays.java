e4bd7485223944872b42a7c57f016b04081502d2
Alex Junior <alexjunior2018@gmail.com>
2025-07-08 00:20:52 -0300

    adicionando exercicio

---
+++ meu-primeiro-programa/src/modulo07/InicializacaoArrays.java
@@ -0,0 +1,23 @@
+package modulo07;
+
+public class InicializacaoArrays {
+    public static void main(String[] args) {
+
+        int idade = 30;
+        int[] numerosImpares = {1, 3, 5, 7, 9};
+
+        String nome = "Fulano da Silva";
+        String telefone1 = "118846687";
+        String telefone2 = "218899875";
+        String[] filhos = {"Zé", "Maria", "Pedro", "Rita"};
+
+        System.out.println(filhos[3]);
+        System.out.println(filhos[2]);
+        System.out.println(filhos[1]);
+        System.out.println(filhos[0]);
+
+        String nomePrimeiroFilho = filhos[0];
+        String nomeSegundoFilho = filhos[1];
+
+    }
+}
