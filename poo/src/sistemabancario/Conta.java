c95c5f789c72af32e7b6f4e2395fb8727a06b3ac
Alex Junior <alexjunior2018@gmail.com>
2025-07-09 14:18:28 -0300

    adicionando exercicio sistemabancario

--- poo/src/sistemabancario/Conta.java
+++ poo/src/sistemabancario/Conta.java
@@ -6,6 +6,7 @@ public class Conta {
     double saldo;
 
     void imprimirSaldo(){
-        System.out.println(this.saldo);
+        System.out.println("Saldo Atual para a conta " + this.numero + " : "
+        + this.saldo);
     }
 }
