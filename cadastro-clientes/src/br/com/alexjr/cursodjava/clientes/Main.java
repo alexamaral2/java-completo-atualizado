4d287319095adbe2e4cf49c56184f3ca77f229d3
Alex Junior <alexjunior2018@gmail.com>
2025-07-17 13:43:53 -0300

    adicionando exercicio

--- cadastro-clientes/src/br/com/alexjr/cursodjava/clientes/Main.java
+++ cadastro-clientes/src/br/com/alexjr/cursodjava/clientes/Main.java
@@ -1,7 +1,12 @@
 package br.com.alexjr.cursodjava.clientes;
 
+import br.com.alexjr.cursodjava.clientes.dominio.Cliente;
+
 public class Main {
     public static void main(String[] args) {
+
+        Cliente cliente = new Cliente();
+        System.out.println(cliente.getCodigo());
 
     }
 }
