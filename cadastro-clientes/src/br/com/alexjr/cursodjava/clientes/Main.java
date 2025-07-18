6b2c580851a7abd27ba29c0d7ad91622035a937b
Alex Junior <alexjunior2018@gmail.com>
2025-07-18 01:08:02 -0300

    adicionando exercicio

--- cadastro-clientes/src/br/com/alexjr/cursodjava/clientes/Main.java
+++ cadastro-clientes/src/br/com/alexjr/cursodjava/clientes/Main.java
@@ -1,12 +1,12 @@
 package br.com.alexjr.cursodjava.clientes;
 
-import br.com.alexjr.cursodjava.clientes.dominio.Cliente;
+import br.com.alexjr.cursodjava.clientes.apresentacao.TelaCadastro;
 
 public class Main {
     public static void main(String[] args) {
 
-        Cliente cliente = new Cliente();
-        System.out.println(cliente.getCodigo());
+        TelaCadastro telaCadastro = new TelaCadastro();
+        telaCadastro.setVisible(true);
 
     }
 }
